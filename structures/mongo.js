a72ca5d942ecb53f4e7f5b0a6284feca91c88dcf
Michael <lewiczkij.m@yandex.ru>
2018-04-14 17:22:03 +0300

    connect

--- structures/mongo.js
+++ structures/mongo.js
@@ -2,15 +2,9 @@
 // Подключение к mongodb для последующей работы других модулей
 "use strict"
 var mongo = require("mongodb");
-// Подключение и используемая база данных
-// if(process.env.MONGODB_URI != undefined) {
-// 	var path = process.env.MONGODB_URI;
-// 	var db = "heroku_f069r4wl"
-// }
-// else{
-	var path = "mongodb://localhost:27017"
-	var db = "test";
-// }
+
+var path = process.env.MONGODB_URI;
+var db = "heroku_f069r4wl"
 
 var connection = mongo.connect(path).
 	then(
